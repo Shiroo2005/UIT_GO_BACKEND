74e1000395b815752e26679ae0f12c3bd3627649
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2025-10-28 22:10:44 +0700

    🚧 build(server): add complete message

--- trip-service/src/main/java/com/se360/UIT_Go/trip_service/services/KafkaProducerService.java
+++ trip-service/src/main/java/com/se360/UIT_Go/trip_service/services/KafkaProducerService.java
@@ -1,6 +1,7 @@
 package com.se360.UIT_Go.trip_service.services;
 
 import com.se360.UIT_Go.trip_service.events.DriverAcceptTripMessage;
+import com.se360.UIT_Go.trip_service.events.DriverCompleteTripMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -11,14 +12,20 @@ import java.util.Map;
 @RequiredArgsConstructor
 @Service
 public class KafkaProducerService {
-    private final String TOPIC = "trip-topic";
+    private final String ACCEPT_TRIP_TOPIC = "accept-trip-topic";
+    private final String COMPLETE_TRIP_TOPIC = "complete-trip-topic";
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void sendTripAcceptMessage(DriverAcceptTripMessage message) {
-        kafkaTemplate.send(TOPIC, message);
+        kafkaTemplate.send(ACCEPT_TRIP_TOPIC, message);
     }
 
-    @KafkaListener(topics = TOPIC, groupId = "mygroup")
+    public void sendTripCompleteMessage(DriverCompleteTripMessage message) {
+        kafkaTemplate.send(COMPLETE_TRIP_TOPIC, message);
+    }
+
+    // test
+    @KafkaListener(topics = ACCEPT_TRIP_TOPIC, groupId = "mygroup")
     public void receiveMessage(DriverAcceptTripMessage message) {
         System.out.println("Receive from kafka: " + message.getDriverId());
     }
