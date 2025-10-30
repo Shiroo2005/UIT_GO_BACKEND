20fa9f8d1824f2f307b4a676544731824857409e
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2025-10-30 13:53:37 +0700

    🚧 build(server): fix composer

--- eureka-server/build.gradle.kts
+++ eureka-server/build.gradle.kts
@@ -21,6 +21,7 @@ repositories {
 extra["springCloudVersion"] = "2025.0.0"
 
 dependencies {
+    implementation ("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
